b4ac302271926eef44a1e7d7848b47b05083a6f4
Arthur Clerc-Gherardi <clerc-gherardi@hotmail.fr>
2019-07-10 19:57:48 +0200

    Some improvement for the display and starting to code rounds

--- gym_buster/envs/buster_env.py
+++ gym_buster/envs/buster_env.py
@@ -1,8 +1,6 @@
 import gym
-import random, string, logging, math, copy
+import random, string, logging, math, sys
 import pygame
-import pygame.gfxdraw
-from pygame.locals import *
 
 logger = logging.getLogger(__name__)
 
@@ -24,6 +22,8 @@ class Constants:
     GHOST_NUMBER = 9
     BUSTER_NUMBER_PER_TEAM = 3
 
+    BASE_RANGE_GHOST_VALIDATED = 1600
+
     GHOST_RUN_WAY = 220
 
     # TYPE
@@ -31,7 +31,18 @@ class Constants:
     TYPE_BUSTER_TEAM_0 = 0
     TYPE_BUSTER_TEAM_1 = 1
 
+    # STATE
+    STATE_GHOST = 0
+    STATE_BUSTER_CARRYING = 1
+    STATE_BUSTER_NOTHING = 0
+
+    # VALUE
+    VALUE_GHOST_BASIC = 0
+    VALUE_BUSTER_NOTHING = -1
+
     # Rendering constants
+    FPS = 30
+
     PYGAME_WINDOW_HEIGHT = 450  # pixels
     PYGAME_WINDOW_WIDTH = 800  # pixels
     PYGAME_WHITE = (255, 255, 255)
@@ -40,7 +51,7 @@ class Constants:
     PYGAME_BUSTER_TEAM_1_COLOR = (255, 0, 0)
     PYGAME_BLACK = (0, 0, 0)
     PYGAME_GHOST_RADIUS = 10  # pixels
-    PYGAME_BUSTER_LENGTH = 10  # pixels
+    PYGAME_BUSTER_RADIUS = 10  # pixels
     PYGAME_SPEED = 1  # pixels
 
     PYGAME_RATIO_HEIGHT = PYGAME_WINDOW_HEIGHT / MAP_HEIGHT
@@ -79,6 +90,25 @@ class MathUtility:
         dist_ratio = length / dist
         return x1 - dist_ratio * (x2 - x1), y1 - dist_ratio * (y2 - y1)
 
+    @staticmethod
+    def distance_from_base_0(ghost):
+        """
+        Function that tells if the ghost is in the base 0
+        :param ghost: the ghost to check
+        :return: a boolean true or false
+        """
+        return MathUtility.distance(0, 0, ghost.x, ghost.y) < Constants.BASE_RANGE_GHOST_VALIDATED
+
+    @staticmethod
+    def distance_from_base_1(ghost):
+        """
+        Function that tells if the ghost is in the base 1
+        :param ghost: the ghost to check
+        :return: a boolean true or false
+        """
+        return MathUtility.distance(Constants.MAP_WIDTH, Constants.MAP_HEIGHT, ghost.x,
+                                    ghost.y) < Constants.BASE_RANGE_GHOST_VALIDATED
+
 
 class Map:
     """
@@ -106,14 +136,14 @@ class Entity:
         Constructor
         """
 
-        self.id = self._generate_id(Constants.ENTITY_ID_LENGTH)
+        self.id = 9999999999
         self.x = Constants.MAP_WIDTH / 2
         self.y = Constants.MAP_HEIGHT / 2
         self.type = type_entity
         self.angle = random.randint(0, 360)
         self.direction = 0
         self.size = 10
-        self.timer = TickTimer(20)
+        self.state = Constants.STATE_BUSTER_NOTHING
 
     def is_in_team_0_base(self):
         """
@@ -130,38 +160,28 @@ class Entity:
         return MathUtility.distance(Constants.MAP_WIDTH + 1, Constants.MAP_HEIGHT + 1, self.x,
                                     self.y) < Constants.ENTITY_RANGE_VISION
 
-    @staticmethod
-    def _generate_id(length):
-        """
-        Function that will generate the id for entities
-        :param length: the length of IDs
-        :return:
-        """
-        result = ""
-        for i in range(length):
-            result += str(random.choice(string.ascii_letters))
-        return result
-
 
 class Ghost(Entity):
     """
     Class that will handle the ghost entity
     """
 
+    ghost_id = 1
+
     def __init__(self, type_entity):
         """
         Constructor
         """
         super(Ghost, self).__init__(type_entity)
-        self._generate_ghost_position()
-        self.image = pygame.transform.rotate(
-            Ghost.create_image(Constants.PYGAME_GHOST_RADIUS, Constants.PYGAME_GHOST_COLOR), self.angle)
-        self.rect = self.image.get_rect()
-        self.rect.topleft = self.convert_position_to_pygame()
-        print("Ghost : " + self.id + ", position : " + str(self.x) + " " + str(self.y) + ", rect : " + str(
-            self.rect) + ", image : " + str(self.image))
+        self.value = Constants.VALUE_GHOST_BASIC
+        self.id = self._generate_id()
+        self._generate_random_ghost_position()
+        self.create_image(Constants.PYGAME_GHOST_COLOR)
+        self.alive = True
+        print("Ghost : " + str(self.id) + ", position : " + str(self.x) + " " + str(self.y) + ", image : " + str(
+            self.image))
 
-    def _generate_ghost_position(self):
+    def _generate_random_ghost_position(self):
         """
         Function that generate a random position for the ghost
         """
@@ -172,12 +192,27 @@ class Ghost(Entity):
             if not (self.is_in_team_0_base() or self.is_in_team_1_base()):
                 generated = True
 
+    @classmethod
+    def _generate_id(cls):
+        """
+        Generate ghosts id
+        :return: the ghost id
+        """
+        ids = cls.ghost_id
+        cls.ghost_id += 1
+        return ids
+
     def draw(self, surface):
         """
         Draw the buster on the surface
         :param surface: the surface where to render the buster image
         """
-        surface.blit(self.image, self.rect)
+        if self.alive:
+            surface.blit(self.image, self.convert_position_to_pygame())
+        else:
+            self.image = pygame.Surface((1, 1)).convert_alpha()
+            self.image.fill((0, 0, 0, 0))
+            surface.blit(self.image, self.convert_position_to_pygame())
 
     def convert_position_to_pygame(self):
         """
@@ -185,16 +220,20 @@ class Ghost(Entity):
         :return: a tuple of converted coordinates
         """
 
-        return (self.x * Constants.PYGAME_RATIO_WIDTH - Constants.PYGAME_GHOST_RADIUS,
-                self.y * Constants.PYGAME_RATIO_HEIGHT - Constants.PYGAME_GHOST_RADIUS)
+        return (round(self.x * Constants.PYGAME_RATIO_WIDTH - Constants.PYGAME_GHOST_RADIUS),
+                round(self.y * Constants.PYGAME_RATIO_HEIGHT - Constants.PYGAME_GHOST_RADIUS))
 
-    @staticmethod
-    def create_image(size, color):
-        image = pygame.Surface((size*2, size*2))
-        image = image.convert_alpha()
+    def create_image(self, color):
+        """
+        Create the first image of the ghost
+        :param color: the color of the ghost
+        """
+        image = pygame.Surface((Constants.PYGAME_GHOST_RADIUS * 2, Constants.PYGAME_GHOST_RADIUS * 2)).convert_alpha()
         image.fill((0, 0, 0, 0))
-        pygame.draw.circle(image, color, (round((size - 1) / 2), round((size - 1) / 2)), round(size / 2))
-        return image
+        pygame.draw.circle(image, color,
+                           (round(Constants.PYGAME_GHOST_RADIUS), round(Constants.PYGAME_GHOST_RADIUS)),
+                           Constants.PYGAME_GHOST_RADIUS)
+        self.image = image
 
     def run_away(self, busters):
         """
@@ -224,19 +263,20 @@ class Buster(Entity):
     """
     Class that will handle busters
     """
+    entity_type_0_id = 1
+    entity_type_1_id = 1
 
-    def __init__(self, type_entity, color):
+    def __init__(self, type_entity):
         """
         Constructor
         """
         super(Buster, self).__init__(type_entity)
+        self.value = Constants.VALUE_BUSTER_NOTHING
         self._generate_buster_position()
-        self.image = pygame.transform.rotate(
-            Buster.create_image(Constants.PYGAME_BUSTER_LENGTH, color), self.angle)
-        self.rect = self.image.get_rect()
-        self.rect.topleft = self.convert_position_to_pygame()
-        print("Buster : " + self.id + ", position : " + str(self.x) + " " + str(self.y) + ", rect : " + str(
-            self.rect) + ", image : " + str(self.image))
+        self.create_image()
+        print(
+            "Buster : " + str(self.id) + ", position : " + str(self.x) + " " + str(self.y) + ", image : " + str(
+                self.image))
 
     def _generate_buster_position(self):
         """
@@ -245,18 +285,36 @@ class Buster(Entity):
         if self.type == Constants.TYPE_BUSTER_TEAM_0:
             self.x = 50
             self.y = 50
+            self.id = self._generate_id(Constants.TYPE_BUSTER_TEAM_0)
         elif self.type == Constants.TYPE_BUSTER_TEAM_1:
             self.x = Constants.MAP_WIDTH - 50
             self.y = Constants.MAP_HEIGHT - 50
+            self.id = self._generate_id(Constants.TYPE_BUSTER_TEAM_1)
         else:
             raise Exception("Entity neither in team 0 or team 1")
+
+    @classmethod
+    def _generate_id(cls, team):
+        """
+        Function that will generate the id for the buster
+        :param team: the entity team
+        :return: the id
+        """
+        if team == Constants.TYPE_BUSTER_TEAM_0:
+            ids = cls.entity_type_0_id
+            cls.entity_type_0_id += 1
+            return ids
+        elif team == Constants.TYPE_BUSTER_TEAM_1:
+            ids = cls.entity_type_1_id
+            cls.entity_type_1_id += 1
+            return ids
 
     def draw(self, surface):
         """
         Draw the buster on the surface
         :param surface: the surface where to render the buster image
         """
-        surface.blit(self.image, self.rect)
+        surface.blit(self.image, self.convert_position_to_pygame())
 
     def convert_position_to_pygame(self):
         """
@@ -264,63 +322,51 @@ class Buster(Entity):
         :return: a tuple of converted coordinates
         """
 
-        return (self.x * Constants.PYGAME_RATIO_WIDTH - Constants.PYGAME_BUSTER_LENGTH/2,
-                self.y * Constants.PYGAME_RATIO_HEIGHT - Constants.PYGAME_BUSTER_LENGTH/2)
+        return (self.x * Constants.PYGAME_RATIO_WIDTH - Constants.PYGAME_BUSTER_RADIUS,
+                self.y * Constants.PYGAME_RATIO_HEIGHT - Constants.PYGAME_BUSTER_RADIUS)
+
+    def create_image(self):
+        """
+        Create the first image of the buster
+        :param color: the color
+        """
+        image = pygame.Surface((Constants.PYGAME_BUSTER_RADIUS * 2, Constants.PYGAME_BUSTER_RADIUS * 2)).convert_alpha()
+        image.fill((0, 0, 0, 0))
+        if self.type == Constants.TYPE_BUSTER_TEAM_0:
+            self.color = Constants.PYGAME_BUSTER_TEAM_0_COLOR
+        else:
+            self.color = Constants.PYGAME_BUSTER_TEAM_1_COLOR
+        pygame.draw.circle(image, self.color,
+                           (round(Constants.PYGAME_BUSTER_RADIUS), round(Constants.PYGAME_BUSTER_RADIUS)),
+                           Constants.PYGAME_BUSTER_RADIUS)
+        self.image = image
 
     @staticmethod
-    def create_image(size, color):
-        image = pygame.Surface((size, size))
-        image = image.convert_alpha()
-        image.fill((0, 0, 0, 0))
-        pygame.draw.rect(image, color, (1, 1, size - 1, size - 1))
-        return image
-
-
-class TickTimer:
-    """
-    Class that will handle ticks for rendering
-    """
-    ticks = 0
-
-    @classmethod
-    def tick(cls):
+    def get_buster(busters, number, team):
         """
-        Function that initialize ticks
+        Retrieve the buster of ID number of team team
+        :param busters: the buster list
+        :param number: the ID number
+        :param team: the team
+        :return: the buster
         """
-        cls.ticks = pygame.time.get_ticks()
-
-    def __init__(self, interval):
-        """
-        Constructor
-        :param interval: the interval between ticks
-        """
-        self.interval = interval
-        self.next_tick = TickTimer.ticks + interval
-
-    def elapse(self):
-        """
-        Function that say if next tick is reached
-        :return: a boolean true or false
-        """
-        if TickTimer.ticks > self.next_tick:
-            self.next_tick += self.interval
-            if TickTimer.ticks > self.next_tick:
-                self.next_tick = TickTimer.ticks + self.interval
-            return True
-        return False
-
+        for buster in busters:
+            if buster.id == number and buster.type == team:
+                return buster
 
 class Game:
     """
     Class that will handle a game
     """
 
-    def __init__(self, mode):
+    def __init__(self, mode, ghost_number, buster_number):
         """
         Constructor
         :param mode: the mode used for the game (console or human)
         """
         self.mode = mode
+        self.ghost_number = ghost_number
+        self.buster_number = buster_number
         self.window_height = Constants.PYGAME_WINDOW_HEIGHT
         self.window_width = Constants.PYGAME_WINDOW_WIDTH
         self._init_screen()
@@ -330,6 +376,8 @@ class Game:
         self.speed = Constants.PYGAME_SPEED
         self.running = True
         self.clock = pygame.time.Clock()
+        self.score_team_0 = 0
+        self.score_team_1 = 0
 
     def reset(self):
         """
@@ -347,17 +395,43 @@ class Game:
         pygame.init()
         self.screen = pygame.display.set_mode(
             (self.window_width, self.window_height))
+        pygame.display.set_caption('Ghost Buster')
+        self._init_writings()
+
+    def _generate_busters(self, buster_number):
+        """
+        Function that will generate all busters
+        :param buster_number: the number of buster in each team
+        """
+        self.busters = []
+        for i in range(buster_number):
+            self.busters.append(Buster(Constants.TYPE_BUSTER_TEAM_0))
+
+        for i in range(buster_number):
+            self.busters.append(Buster(Constants.TYPE_BUSTER_TEAM_1))
+
+    def _generate_ghosts(self, ghost_number):
+        """
+        Function that will generate all ghosts
+        :param ghost_number: the number of ghosts to generate
+        """
+        self.ghosts = []
+        for i in range(ghost_number):
+            self.ghosts.append(Ghost(Constants.TYPE_GHOST))
 
     def loop(self):
         while self.running:
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     self.running = False
-            TickTimer.tick()
             self.game_render()
             pygame.display.flip()
+            self.clock.tick(Constants.FPS)
 
         pygame.quit()
+        sys.exit()
+
+    # RENDERING FUNCTIONS
 
     def game_render(self):
         """
@@ -372,7 +446,54 @@ class Game:
         for ghost in self.ghosts:
             ghost.draw(self.screen)
 
+        self._render_writings()
+
         pygame.display.update()
+
+    def _init_writings(self):
+        """
+        Function called to render the writings on the screen
+        """
+        self.font = pygame.font.Font('freesansbold.ttf', 20)
+        self.text_surface_obj = self.font.render("Team 1 = 0 | Team 2 = 0", True, Constants.PYGAME_WHITE)
+        self.text_rect_obj = self.text_surface_obj.get_rect()
+        self.text_rect_obj.center = (
+            round(Constants.PYGAME_WINDOW_WIDTH * 0.2), round(Constants.PYGAME_WINDOW_HEIGHT * 0.9))
+
+    def _render_writings(self):
+        """
+        Function where score is evolving and redering it in text on screen
+        """
+        self.text_surface_obj = self.font.render(
+            "Team 1 = " + str(self.score_team_0) + " | Team 2 = " + str(self.score_team_1), True,
+            Constants.PYGAME_WHITE)
+        self.text_rect_obj = self.text_surface_obj.get_rect()
+        self.text_rect_obj.center = (
+            round(Constants.PYGAME_WINDOW_WIDTH * 0.2), round(Constants.PYGAME_WINDOW_HEIGHT * 0.9))
+        self.screen.blit(self.text_surface_obj, self.text_rect_obj)
+
+    # GAME FUNCTIONS
+
+    def _run_round(self):
+        """
+        Function that will run a round and execute each event triggered
+        """
+        # First execute automatic actions
+
+        # Ghost released in a base
+        for ghost in self.ghosts:
+            if MathUtility.distance_from_base_0(ghost):
+                self.score_team_0 += 1
+                ghost.alive = False
+            elif MathUtility.distance_from_base_1(ghost):
+                self.score_team_1 += 1
+                ghost.alive = False
+
+        # Execute for each buster his tasks
+        for i in range(1, self.buster_number + 1):
+            # Retrieve busters i to execute their action at the same moment (almost)
+            buster_team_0 = Buster.get_buster(self.busters, i, Constants.TYPE_BUSTER_TEAM_0)
+            buster_team_1 = Buster.get_buster(self.busters, i, Constants.TYPE_BUSTER_TEAM_1)
 
     def _update_entities_position(self):
         """
@@ -383,27 +504,6 @@ class Game:
 
         for buster in self.busters:
             self.board.map[buster.x][buster.y] = buster.type
-
-    def _generate_busters(self, buster_number):
-        """
-        Function that will generate all busters
-        :param buster_number: the number of buster in each team
-        """
-        self.busters = []
-        for i in range(buster_number):
-            self.busters.append(Buster(Constants.TYPE_BUSTER_TEAM_0, Constants.PYGAME_BUSTER_TEAM_0_COLOR))
-
-        for i in range(buster_number):
-            self.busters.append(Buster(Constants.TYPE_BUSTER_TEAM_1, Constants.PYGAME_BUSTER_TEAM_1_COLOR))
-
-    def _generate_ghosts(self, ghost_number):
-        """
-        Function that will generate all ghosts
-        :param ghost_number: the number of ghosts to generate
-        """
-        self.ghosts = []
-        for i in range(ghost_number):
-            self.ghosts.append(Ghost(Constants.TYPE_GHOST))
 
 
 class BusterEnv(gym.Env):
@@ -426,5 +526,5 @@ class BusterEnv(gym.Env):
 
 
 if __name__ == '__main__':
-    game = Game('human')
+    game = Game('human', Constants.GHOST_NUMBER, Constants.BUSTER_NUMBER_PER_TEAM)
     game.loop()
