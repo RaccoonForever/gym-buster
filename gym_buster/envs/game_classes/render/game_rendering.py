e9cdc2b62fbd41370e641e183a18bb750569113d
rynn69270 <clerc-gherardi@hotmail.fr>
2019-08-06 18:05:01 +0200

    new file game rendering

---
+++ gym_buster/envs/game_classes/render/game_rendering.py
@@ -0,0 +1,33 @@
+import pygame
+
+class GameRendering:
+    """
+    Class that will handle every render object for the game
+    """
+    
+    def __init__(self, window_width, window_height):
+        """
+        Constructor
+        """
+        self.window_width = window_width
+        self.window_height = window_height
+    
+    def _init_screen(self):
+        """
+        Function that will initialize the screen
+        """
+        pygame.init()
+        self.screen = pygame.display.set_mode(
+            (self.window_width, self.window_height))
+        pygame.display.set_caption('Ghost Buster')
+        self._init_writings()
+     
+    def _init_writings(self):
+        """
+        Function called to render the writings on the screen
+        """
+        self.font = pygame.font.Font('freesansbold.ttf', 20)
+        self.text_surface_obj = self.font.render("Team 1 = 0 | Team 2 = 0", True, Constants.PYGAME_WHITE)
+        self.text_rect_obj = self.text_surface_obj.get_rect()
+        self.text_rect_obj.center = (
+            round(Constants.PYGAME_WINDOW_WIDTH * 0.2), round(Constants.PYGAME_WINDOW_HEIGHT * 0.9))
