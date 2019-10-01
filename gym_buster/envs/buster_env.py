b15fd335aa29665fd509158cd9c94b873f614750
RaccoonForever <clerc-gherardi@hotmail.fr>
2019-10-01 16:51:24 +0200

    reorganizing imports

--- gym_buster/envs/buster_env.py
+++ gym_buster/envs/buster_env.py
@@ -1,10 +1,11 @@
-import gym
-from gym import spaces
-from gym.utils import seeding
-
-from gym.envs.classic_control import rendering
 import random
 import numpy as np
+import gym
+
+from gym import spaces
+from gym.utils import seeding
+from gym.envs.classic_control import rendering
+
 from gym_buster.envs.game_classes.constants import Constants
 from gym_buster.envs.game_classes.buster import Buster
 from gym_buster.envs.game_classes.ghost import Ghost
